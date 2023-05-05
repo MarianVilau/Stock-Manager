251bf832b1845666169621abd353db35787a2544
Marian Vilau <79789514+MarianVilau@users.noreply.github.com>
2023-05-05 18:42:30 +0300

    bug version

--- includes/load.php
+++ includes/load.php
@@ -17,8 +17,8 @@ const LIB_PATH_INC = SITE_ROOT . DS;
 require_once(LIB_PATH_INC . 'config.php');
 require_once(LIB_PATH_INC . 'functions.php');
 require_once(LIB_PATH_INC.'Session.php');
-require_once(LIB_PATH_INC.'Media.php');
-require_once(LIB_PATH_INC.'MySqli_DB.php');
+require_once(LIB_PATH_INC.'upload.php');
+require_once(LIB_PATH_INC.'database.php');
 require_once(LIB_PATH_INC . 'sql.php');
 
 ?>
---
+++ layouts/admin_menu.php
@@ -0,0 +1,51 @@
+<div class="menu-items">
+    <ul class="nav-links">
+        <li><a href="admin.php">
+                <i class="uil uil-dashboard"></i>
+                <span class="link-name">Dashboard</span>
+            </a></li>
+        <li><a href="group.php">
+                <i class="uil uil-users-alt"></i>
+                <span class="link-name">Manage Groups</span>
+            </a></li>
+        <li><a href="users.php">
+                <i class="uil uil-user"></i>
+                <span class="link-name">Manage Users</span>
+            </a></li>
+        <li><a href="categorie.php">
+                <i class="uil uil-list-ul"></i>
+                <span class="link-name">Categories</span>
+            </a></li>
+        <li><a href="product.php">
+                <i class="uil uil-apps"></i>
+                <span class="link-name">Manage Products</span>
+            </a></li>
+        <li><a href="add_product.php">
+                <i class="uil uil-plus-circle"></i>
+                <span class="link-name">Add Products</span>
+            </a></li>
+        <li><a href="media.php">
+                <i class="uil uil-images"></i>
+                <span class="link-name">Media Files</span>
+            </a></li>
+        <li><a href="sales.php">
+                <i class="uil uil-usd-circle"></i>
+                <span class="link-name">Manage Sales</span>
+            </a></li>
+        <li><a href="add_sale.php">
+                <i class="uil uil-dollar-sign-alt"></i>
+                <span class="link-name">Add Sales</span>
+            </a></li>
+        <li><a href="sales_report.php">
+                <i class="uil uil-bill"></i>
+                <span class="link-name">Sales by dates</span>
+            </a></li>
+        <li><a href="monthly_sales.php">
+                <i class="uil uil-calendar-alt"></i>
+                <span class="link-name">Monthly sales</span>
+            </a></li>
+        <li><a href="daily_sales.php">
+                <i class="uil uil-calender"></i>
+                <span class="link-name">Daily sales</span>
+            </a></li>
+    </ul>
